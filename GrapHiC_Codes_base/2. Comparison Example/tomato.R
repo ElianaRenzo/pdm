d169ad47ba7a92a79077fce2e24d0dfd6fb93cbf
ElianaRenzo <eliana.renzo@epfl.ch>
2021-03-18 15:24:26 +0100

    First commit

---
+++ GrapHiC_Codes_base/2. Comparison Example/tomato.R
@@ -0,0 +1,20 @@
+library(RSMLM)
+setwd('/Users/Eliana/Documents/PDM/GrapHiC_Codes_Eliana/2.\ Comparison\ Example/Data')
+
+for (r_seed in 0:49) {
+  for (i in 0:13) {
+    name = paste("data", r_seed, "_", i, sep = "")
+    data <- read.csv(paste(name, ".csv", sep=""), header = FALSE)
+    # search radius
+    tomatoR <- 8
+    # persistence threshold
+    tomatoThresh <- 5
+    # cluster data using ToMATo
+    labels <- clusterTomato(data, tomatoR, tomatoThresh)
+    write.csv(labels, paste("tomato", name, ".csv", sep=""))
+  }
+}
+
+
+
+
