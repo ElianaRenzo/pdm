03529bc3d7c3adb2b6b94f0c34dea224e6074d58
ElianaRenzo <eliana.renzo@epfl.ch>
2021-05-06 17:24:01 +0200

    Sortir Graphic_codes_base du git

--- My_codes/tomato.R
+++ My_codes/tomato.R
@@ -1,40 +1,46 @@
 library(RSMLM)
 setwd('/Users/Eliana/Documents/PDM/Codes/My_codes/Data')
 
-ID <- '3'
+folder_name <- 'different_densities/'
+ID <- '4'
 
-data <- read.csv(paste('simulated_SMLM_', ID, '.csv', sep = ""))
+
 # search radius
 tomatoR <- 31 # Selon juliette, mettre plutot un peu plus que 30 (triche)
 # persistence threshold
 tomatoThresh <- 6
 
-detectionList <- data
-#data.xy <- subset(data, select=c("V1", "V2"))
 
-coords <- as.matrix(detectionList[, c('x', 'y')])
-
-# cluster data using ToMATo
-labels <- clusterTomato(coords, tomatoR, tomatoThresh)
-#write.csv(labels, paste("tomato", name, ".csv", sep=""))
-
-# Tomato result
-plotClusterScatter(coords, labels)
-
-# GT
-plotClusterScatter(coords, detectionList$labels_1)
-
-numClustersTom <- sum(unique(labels) > 0)
-numClustersGT <- sum(unique(detectionList$labels_1) > 0)
-
-print(paste('ToMATo found', numClustersTom, 'clusters in the dataset. The ground truth number is', numClustersGT))
-
-tomatoDiag <- tomatoDiagram(coords, tomatoR)
-plotTomatoDiagram(tomatoDiag, tomatoThresh)
-
-filename <- paste("tomato_result_", ID, ".csv", sep = "")
-
-# write.csv(labels, filename)
+for (i in 0:29) {
+  data <- read.csv(paste(folder_name, 'simulated_SMLM_', ID, '_', i, '.csv', sep = ""))
+  
+  detectionList <- data
+  #data.xy <- subset(data, select=c("V1", "V2"))
+  
+  coords <- as.matrix(detectionList[, c('x', 'y')])
+  
+  # cluster data using ToMATo
+  labels <- clusterTomato(coords, tomatoR, tomatoThresh)
+  #write.csv(labels, paste("tomato", name, ".csv", sep=""))
+  
+  # Tomato result
+  plotClusterScatter(coords, labels)
+  
+  # GT
+  plotClusterScatter(coords, detectionList$labels_1)
+  
+  numClustersTom <- sum(unique(labels) > 0)
+  numClustersGT <- sum(unique(detectionList$labels_1) > 0)
+  
+  print(paste('ToMATo found', numClustersTom, 'clusters in the dataset. The ground truth number is', numClustersGT))
+  
+  tomatoDiag <- tomatoDiagram(coords, tomatoR)
+  plotTomatoDiagram(tomatoDiag, tomatoThresh)
+  
+  filename <- paste("tomato_results/tomato_result_", ID, "_", i , ".csv", sep = "")
+  
+  write.csv(labels, filename)
+}
 
 
 
