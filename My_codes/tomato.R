29e32f6c838677459f7182f610bc12f9f6787bef
ElianaRenzo <eliana.renzo@epfl.ch>
2021-07-05 17:21:25 +0200

    améliorer des figures et re run Power Of Seperation pour avoir le stdev

--- My_codes/tomato.R
+++ My_codes/tomato.R
@@ -3,14 +3,14 @@ library(RSMLM)
 setwd('/Users/Eliana/Documents/PDM/Codes/My_codes/Deviations_from_standard/')
 
 
-folder_name <- 'high_noise/'
-ID <- '3'
+folder_name <- 'standard/'
+ID <- '1'
 
 
 # search radius
 tomatoR <- 35 # Selon juliette, mettre plutot un peu plus que 30 (triche)
 # persistence threshold
-tomatoThresh <- 8
+tomatoThresh <- 5
 
 NumberOfSimulations <- 30
 
